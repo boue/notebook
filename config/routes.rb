be87d88435ef49fa683bc3031ab0b4a5dcffec9b
Germain Boue <boue.germain@gmail.com>
2015-01-20 18:16:06 -0800

    Add the css files. Copy pasted latest normalize

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
+  resources :notes
+
+  #if user is signed_in / can't have two roots in rails so called it a different name (you can have marketing view and then the dashboard)
+  authenticated :user do 
+    root 'notes#index', as: "authenticated_root"
+  end
 
   root "welcome#index"
 
-  resources :notes
+
 end
